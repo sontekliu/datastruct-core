7868d4f6bbf66e628cee8c46267bb6e0324735cc
sontekliu <sontek@yeah.net>
2019-09-18 11:27:06 +0800

    Add some comment

--- src/main/java/com/javaliu/collection/Array.java
+++ src/main/java/com/javaliu/collection/Array.java
@@ -12,23 +12,42 @@ public class Array {
     private int[] data;
     private int size;
 
+    /**
+     * 构造数组对象
+     * @param capacity      初始化时的容量
+     */
     public Array(int capacity) {
         data = new int[capacity];
         size = 0;
     }
 
+    /**
+     * 默认的构造方法，初始化容量为 10
+     */
     public Array() {
         this(10);
     }
 
+    /**
+     * 数组的大小
+     * @return  数组的大小
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * 数组所能容纳的数据的多少
+     * @return
+     */
     public int getCapacity() {
         return data.length;
     }
 
+    /**
+     * 数组是否为空
+     * @return      true:空, false:非空
+     */
     public boolean isEmpty() {
         return size == 0;
     }
